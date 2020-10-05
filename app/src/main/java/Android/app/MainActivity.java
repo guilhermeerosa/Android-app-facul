a39ddac1239a204b89eb0d24769188ef2fd5d4a9
Guilherme Rosa <59878498+guilhermeerosa@users.noreply.github.com>
2020-10-05 12:04:12 -0300

    Finish Version

--- app/src/main/java/Android/app/MainActivity.java
+++ app/src/main/java/Android/app/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText editName, editAge;
-//    private Button show, clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         editName = findViewById(R.id.et_name);
         editAge = findViewById(R.id.et_age);
-//        Button show = (Button) findViewById(R.id.btnShow);
-//        Button clear = (Button) findViewById(R.id.btnClear);
     }
 
     public void onClickBtnShow(View v){
-//        EditText editName = findViewById(R.id.et_name);
-//        EditText editAge = findViewById(R.id.et_age);
-
-//        String text1 = editName.getText().toString();
-//        String text2 = editAge.getText().toString();
-//        String message = "Olá " + text1 + ", você tem " + text2 + " anos.";
         String message = "Olá " + editName.getText() + ", você tem " + editAge.getText() + " anos.";
 
-//        Toast.makeText(MainActivity.this, (editName).getText(), Toast.LENGTH_LONG).show();
         Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
     }
 
